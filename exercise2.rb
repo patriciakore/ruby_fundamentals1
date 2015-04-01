60433e0874695588c10484950c8af22faa7aeb02
Patricia Kore <patricia.kore@yahoo.com>
2015-03-31 22:26:36 -0400

    exercise2 ruby

---
+++ exercise2.rb
@@ -0,0 +1,28 @@
+#tip for 55 dollar meal
+
+x=55
+tip=0.2*55
+puts "the tip is #{tip}"
+
+
+#adding a string and an integer
+
+string="my_first_ruby_string"
+x=5
+string2=string+x.to_s
+
+puts "The string: #{string} x: #{x}  the addition: #{string2}"
+
+
+#multiplication 
+a=45628
+b=7839
+puts "the result of the multiplication is #{a*b}"
+
+
+#booleans 
+
+
+expression=(true && false) || (false && true) || !(false && false)
+
+puts "the value of the expression evaluates to #{expression}"
