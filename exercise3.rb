150db5f86c68150664341c8b97648c093641482e
Patricia Kore <patricia.kore@yahoo.com>
2015-03-31 22:47:25 -0400

    exercise 3 ruby , the user has entered their age

--- exercise3.rb
+++ exercise3.rb
@@ -3,3 +3,11 @@ puts "What is your name?"
 name=gets.strip
 
 puts "Hi #{name}!"
+
+puts "How old are you?"
+
+age=gets.strip
+
+birth_year=2015-age.to_i
+
+puts "You were born in year #{birth_year}"
